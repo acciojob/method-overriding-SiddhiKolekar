0f8c1cc352de4a5f84f45c0ae0734c9f830af2d2
Siddhi <siddhikolekar2018@gmaiil.com>
2023-11-22 16:10:20 +0530

    Siddhi

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+
 public class Main {
-  
+    public static class A{
+        public void meth(){
+            System.out.println("Invoking method from class A");
+        }
+    }
+    public static class B extends A{
+        @Override
+        public void meth() {
+            System.out.println("Method is overridden in Extendend class B");
+        }
+    }
+    public static void main(String[] args) {
+        B b = new B();
+        b.meth();
+    }
 }
