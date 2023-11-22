ab138ae0609224a4c5d42f259a24478d2359c0b9
SiddhiKolekar <94230227+SiddhiKolekar@users.noreply.github.com>
2023-11-22 21:20:45 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,13 +8,16 @@ public class Main {
         }
     }
     public static class B extends A{
-        @Override
-        public void meth() {
+        
+        public String meth() {
             System.out.println("Method is overridden in Extendend class B");
         }
     }
     public static void main(String[] args) {
         B b = new B();
-        b.meth();
+        System.out.println(b.meth());
+
+        B b1 = new B();
+        System.out.println(b.meth());
     }
 }
